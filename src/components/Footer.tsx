f3ea2b6ec2e97506b801e2d33e65c166f5ed88e5
Claire Karsenti <claire.karsenti.web@gmail.com>
2022-12-19 20:08:02 +0100

    :recycle: Added testid to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
           const { id, url, img, text } = social;
           return (
             <li key={id}>
-              <a href={url}>
+              <a href={url} data-testid={text}>
                 <img src={img} alt={text} />
               </a>
             </li>
@@ -42,11 +42,17 @@ const Footer = () => {
             href="https://www.frontendmentor.io/challenges/loopstudios-landing-page-N88J5Onjw/hub"
             target="_blank"
             rel="noreferrer"
+            data-testid="attribute-frontend"
           >
             Frontend Mentor
           </a>
           . Coded with <span className="heart">🖤</span> by{' '}
-          <a href="https://www.linkedin.com/in/claire-karsenti/">
+          <a
+            href="https://www.linkedin.com/in/claire-karsenti/"
+            target="_blank"
+            rel="noreferrer"
+            data-testid="attribute-linkedin"
+          >
             Claire Karsenti
           </a>
           .
